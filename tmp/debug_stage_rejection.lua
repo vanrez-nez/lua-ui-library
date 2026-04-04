dc4bc323d2ad6e47fe3976e4c070b28a0575a34e
Ivan Juarez <ivan@blindgazer.com>
2026-04-03 20:30:43 -0600

    feat: clean tmp file

--- tmp/debug_stage_rejection.lua
+++
@@ -1,9 +0,0 @@
-local Stage = require('lib.ui.scene.stage')
-local ok, err = pcall(function()
-    Stage({ unsupportedProp = true })
-end)
-if ok then
-    print("SUCCESS (FAIL)")
-else
-    print("FAILED AS EXPECTED: " .. tostring(err))
-end
