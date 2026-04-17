437a31cecd211596799cde9d03a94c6a344c1bab
Ivan Juarez <ivan@blindgazer.com>
2026-04-16 21:48:25 -0600

    feat: proper clone table util

--- lib/ui/utils/common.lua
+++ lib/ui/utils/common.lua
@@ -1,14 +1,14 @@
 local Utils = {}
 
 function Utils.copy_array(values)
-    if values == nil then
-        return nil
-    end
-    local copy = {}
-    for index = 1, #values do
-        copy[index] = values[index]
-    end
-    return copy
+  if values == nil then
+      return nil
+  end
+  local copy = {}
+  for index = 1, #values do
+      copy[index] = values[index]
+  end
+  return copy
 end
 
 function Utils.copy_table(values)
@@ -20,6 +20,53 @@ function Utils.copy_table(values)
         copy[key] = value
     end
     return copy
+end
+
+function Utils.clone(obj, deep, _visited)
+  local out = setmetatable({}, getmetatable(obj))
+  if not deep then
+    for k, v in pairs(obj) do out[k] = v end
+    return out
+  end
+
+  _visited = _visited or {}
+  if _visited[obj] then return _visited[obj] end
+  _visited[obj] = out
+
+  for k, v in pairs(obj) do
+    local ck = type(k) == 'table' and Utils.clone(k, true, _visited) or k
+    local cv = type(v) == 'table' and Utils.clone(v, true, _visited) or v
+    out[ck] = cv
+  end
+
+  return out
+end
+
+function Utils.merge(target, source, deep, overwrite, _visited)
+  if not deep then
+    for k, v in pairs(source) do
+      if overwrite or target[k] == nil then target[k] = v end
+    end
+    return target
+  end
+
+  _visited = _visited or {}
+  if _visited[source] then return target end
+  _visited[source] = true
+
+  for k, v in pairs(source) do
+    if type(v) == 'table' then
+      if type(target[k]) == 'table' then
+        Utils.merge(target[k], v, true, overwrite, _visited)
+      elseif overwrite or target[k] == nil then
+        target[k] = Utils.clone(v, true, _visited)
+      end
+    elseif overwrite or target[k] == nil then
+      target[k] = v
+    end
+  end
+
+  return target
 end
 
 function Utils.merge_tables(target, source)
